1eacf7f50f6f01226ed5884e6f365c0659cf277c
小书童开发者 <dev@xiaoshutong.com>
2026-07-08 23:15:47 +0000

    fix: 增大 express.json 请求体限制至 50mb，支持多图 Base64 上传

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ const voiceUpload = multer({
 
 // 中间件
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/voices', express.static(path.join(__dirname, 'voices')));
 
